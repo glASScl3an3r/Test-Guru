a4bbdc68642fcac9d76f1e2297d0474ce9018026
glASScl3an3r <vl_gladskoy@mail.ru>
2021-05-24 16:43:58 -0400

    валидации и тексты ошибок на неск. языках

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :test
 
+  validates :text, presence: true
+
   has_many :answers, dependent: :destroy
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,13 @@ class Test < ApplicationRecord
   has_many :users, through: :passed_tests
   has_many :passed_tests, dependent: :destroy
 
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+  validates :level, presence: true,
+                    numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 0
+                                  }
+
   scope :easy, -> { where(level: 0..1).order(created_at: :desc) }
   scope :medium, -> { where(level: 2..4).order(created_at: :desc) }
   scope :hard, -> { where(level: 5..Float::INFINITY).order(created_at: :desc) }
