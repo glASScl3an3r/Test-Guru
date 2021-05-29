3b11f98f9791cc9edb8146958c446a0d263464b5
glASScl3an3r <vl_gladskoy@mail.ru>
2021-05-29 12:35:23 -0400

    small code style fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,15 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :right, -> { where(correct: true) }
-
   validates :text, presence: true
   validates :correct,  inclusion: { in: [true, false] }
   validate :validate_answers_count, on: :create
 
+  scope :right, -> { where(correct: true) }
+
   private
 
   def validate_answers_count
-    errors.add(:question, :too_many_answers) if question.present? && question.answers.count >= 4
+    errors.add(:question, :too_many_answers) if question.answers.count >= 4
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,16 +6,17 @@ class Test < ApplicationRecord
   has_many :users, through: :passed_tests
   has_many :passed_tests, dependent: :destroy
 
+  validates :title, presence: true
+  validates :level, presence: true,
+                    numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 0 }
+
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :by_level, -> (level) { where(level: level) }
   scope :by_category, -> (category) { joins(:category).
                                       where('categories.title = ?', category) }
-  validates :title, presence: true
-  validates :level, presence: true,
-                    numericality: { only_integer: true,
-                                    greater_than_or_equal_to: 0 }
 
   def self.sorted_by_category(category)
     by_category(category)
