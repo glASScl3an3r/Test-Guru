9a7ed6626af87a359cd8452afa841c25839a2012
glASScl3an3r <vl_gladskoy@mail.ru>
2021-06-20 00:30:59 -0400

    views

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question)
+    if question.new_record?
+      "<h1>test #{question.test.title}: Create new question</h1>".html_safe
+    else
+      "<h1>test #{question.test.title}: Edit question</h1>".html_safe
+    end
+  end
 end
