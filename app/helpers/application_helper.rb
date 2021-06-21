6d56644b8247e788daab57a50034d9f39bf70bd0
glASScl3an3r <vl_gladskoy@mail.ru>
2021-06-21 08:13:03 -0400

    some fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
   def github_url(author, repo)
-    "https://github.com/#{author}/#{repo}"
+    link_to 'Test-Guru',
+            "https://github.com/#{author}/#{repo}",
+            target: :_blank,
+            rel: :nofollow
   end
 
   def current_year
-    Time.now.year
+    Time.current.year
   end
 end
