1a914165846d4f45355a43c10e56d72114cc2270
glASScl3an3r <vl_gladskoy@mail.ru>
2021-07-17 06:01:54 -0400

    Строгий выбор из существующих правил и пикч + их валидация

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -6,11 +6,40 @@ class Badge < ApplicationRecord
   validates :image_name, presence: true
   validates :rule, presence: true
   validate :image_exist, on: %i[create update]
+  validate :rule_is_available, on: %i[create update]
+
+  #можно вне класса Badge сделать, но пусть будет так
+  BADGE_RULES_PATH = 'app/services/specifications/badges/*.rb'.freeze
+  def self.available_rules
+    if @available_rules_strings.nil?
+      @available_rules_strings = []
+      Dir.glob(BADGE_RULES_PATH) do |file|
+        @available_rules_strings << File.basename(file, '.rb')
+      end
+    end
+
+    @available_rules_strings
+  end
+
+  BADGE_IMAGES_PATH = 'app/assets/images/badges/*'
+  def self.available_images
+    if @available_images_strings.nil?
+      @available_images_strings = []
+      Dir.glob(BADGE_IMAGES_PATH) do |file|
+        @available_images_strings << File.basename(file)
+      end
+    end
+
+    @available_images_strings
+  end
 
   private
 
   def image_exist
-    img = ActionController::Base.helpers.resolve_asset_path("badges/#{image_name}")
-    errors.add(:image_name, :file_missing) if img.nil?
+    errors.add(:image_name, :file_missing) if !self.class.available_images.include?(image_name)
+  end
+
+  def rule_is_available
+    errors.add(:rule, :rule_is_invalid) if !self.class.available_rules.include?(rule)
   end
 end
